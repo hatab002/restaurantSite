1715aef09cdb6d14e81545046d2f9f921ab03d95
hatab002 <hatab002@umn.edu>
2019-01-13 16:31:39 -0600

    validate data working

--- restaurant.js
+++ restaurant.js
@@ -3,11 +3,11 @@ function validate (){
     var name = document.getElementById("inputName").value
     var email = document.getElementById("inputEmail").value
     var phone = document.getElementById("inputPhone").value
-
     var checkbox = document.querySelectorAll("input.checkbox:checked")
+    var isnum = /^\d+$/.test(phone)
 
-    if (name == "" || email == "" || phone == "" || checkbox.length === 0 ){
-        alert('Please fill all fields before submitting')
+    if (name == "" || email == "" || phone == "" || checkbox.length === 0 || isnum == false){
+        alert('Please fill all fields correctly before submitting')
     } else {
         alert('Thanks for contacting us, someone will be in touch with you shortly')
     }
